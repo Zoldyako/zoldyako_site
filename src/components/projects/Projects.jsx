f3ba3f01a88730a64d6e96dc0645bd920b863b01
José Victor <jose.vdr.pessoal@gmail.com>
2025-02-27 11:51:02 -0300

    Updated Projects component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,43 +1,13 @@
 import styles from './Projects.module.css'
 
-export default function Project() {
+export default function Project({name, desc, img}) {
     return (
         <>
-            <div className={styles.container}>
-                <div className={styles.project}>
-                    <h2>Project tile</h2>
-                    <div className={styles.desc}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, minima placeat? Quis repellendus esse qui. Consequatur, perspiciatis assumenda laudantium quis itaque vero, commodi quasi tempora officia, molestias minus eius. Veniam?</p>
-                        <img src="https://placehold.co/150/png" alt="Projects preview image" />
-                    </div>
-                </div>
-                <div className={styles.project}>
-                    <h2>Project tile</h2>
-                    <div className={styles.desc}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, minima placeat? Quis repellendus esse qui. Consequatur, perspiciatis assumenda laudantium quis itaque vero, commodi quasi tempora officia, molestias minus eius. Veniam?</p>
-                        <img src="https://placehold.co/150/png" alt="Projects preview image" />
-                    </div>
-                </div>
-                <div className={styles.project}>
-                    <h2>Project tile</h2>
-                    <div className={styles.desc}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, minima placeat? Quis repellendus esse qui. Consequatur, perspiciatis assumenda laudantium quis itaque vero, commodi quasi tempora officia, molestias minus eius. Veniam?</p>
-                        <img src="https://placehold.co/150/png" alt="Projects preview image" />
-                    </div>
-                </div>
-                <div className={styles.project}>
-                    <h2>Project tile</h2>
-                    <div className={styles.desc}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, minima placeat? Quis repellendus esse qui. Consequatur, perspiciatis assumenda laudantium quis itaque vero, commodi quasi tempora officia, molestias minus eius. Veniam?</p>
-                        <img src="https://placehold.co/150/png" alt="Projects preview image" />
-                    </div>
-                </div>
-                <div className={styles.project}>
-                    <h2>Project tile</h2>
-                    <div className={styles.desc}>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, minima placeat? Quis repellendus esse qui. Consequatur, perspiciatis assumenda laudantium quis itaque vero, commodi quasi tempora officia, molestias minus eius. Veniam?</p>
-                        <img src="https://placehold.co/150/png" alt="Projects preview image" />
-                    </div>
+            <div className={styles.project_component}>
+                <h2>{name}</h2>
+                <div className={styles.desc}>
+                    <p>{desc}</p>
+                    <img src={img} alt="Projects preview image" />
                 </div>
             </div>
         </>
