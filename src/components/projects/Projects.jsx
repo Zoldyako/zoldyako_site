bb90490b4e5e136237c9bf2594c340efa2972cc2
José Victor <jose.vdr.pessoal@gmail.com>
2025-03-03 20:38:41 -0300

    Added link to project to component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,15 @@
 import styles from './Projects.module.css'
 
-export default function Project({name, desc, img}) {
+export default function Project({name, desc, img, link}) {
     return (
         <>
             <div className={styles.project_component}>
                 <h2>{name}</h2>
                 <div className={styles.desc}>
-                    <p>{desc}</p>
+                    <div>
+                        <p>{desc}</p>
+                        <p><a href={link} target="_blank">Link to this project</a></p>
+                    </div>
                     <img src={img} alt="Projects preview image" />
                 </div>
             </div>
