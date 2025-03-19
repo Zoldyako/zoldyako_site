71f06a5e2dffb9f286d44e1ae1b86ca146a64d46
José Victor <jose.vdr.pessoal@gmail.com>
2025-03-19 20:52:57 -0300

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import 'normalize.css' 
+import 'normalize.css'
 import './App.css'
 import Sidebar from './components/sidebar/Sidebar'
 import Projects from './components/projects/Projects'
@@ -9,23 +9,23 @@ function App() {
 
   return (
     <>
-      <Sidebar />
-      <div className='projects-container'>
-        <Projects 
-          name="Etch a Sketch"
-          desc="The Odin Project's etch a sketch is a challenge meant to put to work the concepts that were taught up to that point in the course."
-          img={project_one}
-          link="https://github.com/Zoldyako/etch-a-sketch"
-        />
-        <Projects 
-          name="My Personal site (this one :P)"
-          desc="A basic site made to display my current skills with programming. Right now it is made with React/Vite."
-          img={perfil}
-          link="https://github.com/Zoldyako/zoldyako_site"
-        />
-      </div>
+      <Sidebar/>
+        <div className='projects-container '>
+          <Projects
+            name="Etch a Sketch"
+            desc="The Odin Project's etch a sketch is a challenge meant to put to work the concepts that were taught up to that point in the course."
+            img={project_one}
+            link="https://github.com/Zoldyako/etch-a-sketch"
+          />
+          <Projects
+            name="My Personal site (this one :P)"
+            desc="A basic site made to display my current skills with programming. Right now it is made with React/Vite."
+            img={perfil}
+            link="https://github.com/Zoldyako/zoldyako_site"
+          />
+        </div>
     </>
-  )
+  );
 }
 
 export default App
