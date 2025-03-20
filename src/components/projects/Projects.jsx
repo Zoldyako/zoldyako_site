fade253713c72d04445d6511338e646e5697f0d4
José Victor <jose.vdr.pessoal@gmail.com>
2025-03-20 01:23:53 -0300

    Rewrite of the Projects.jsx component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,16 +3,15 @@ import styles from './Projects.module.css'
 export default function Project({name, desc, img, link}) {
     return (
         <>
-            <div className={styles.project_component}>
-                <h2>{name}</h2>
-                <div className={styles.desc}>
-                    <div>
-                        <p>{desc}</p>
-                        <p><a href={link} target="_blank">Link to this project</a></p>
+            <a href={link} target="_blank">
+                <div className={styles.project_component}>
+                    <div className={styles.about}>
+                        <h2 className={styles.title}>{name}</h2>
+                        <p className={styles.desc}>{desc}</p>
                     </div>
-                    <img src={img} alt="Projects preview image" />
+                    <img className={styles.preview} src={img} alt="Projects preview image" />
                 </div>
-            </div>
+            </a>
         </>
     )
 }
