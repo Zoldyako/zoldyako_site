935bc679fda4e9eb0f1611f67d46b5013e2608c1
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-03-31 02:11:29 -0300

    update: configs

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        remotePatterns: [new URL('https://placehold.co/**')]
-    }
+        remotePatterns: [new URL("https://placehold.co/**")],
+    },
 };
 
 export default nextConfig;
