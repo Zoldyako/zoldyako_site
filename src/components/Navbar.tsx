4544f7ab1f3eff82b1fb193ef39ce30c06630a19
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-03-31 02:26:33 -0300

    add: lucide-icons, update: Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,13 @@
-import Image from "next/image";
+import { House } from "lucide-react";
 
 export default function Navbar() {
     return (
-        <div className="w-18 min-w-18 h-full flex flex-col gap-4 items-center py-4 bg-zinc-800 border-r border-zinc-600">
+        <div className="w-18 min-w-18 h-svh flex flex-col gap-4 items-center py-6 bg-zinc-800 border-r border-zinc-600">
             <a href="#">
                 <div>
-                    <Image
-                        width={48}
-                        height={48}
-                        src="https://placehold.co/48x48/png"
-                        alt="placeholder"
-                        loading="lazy"
-                        className="rounded"
-                    />
+                    <button className="hover:cursor-pointer">
+                        <House size={28} strokeWidth={2} />
+                    </button>
                 </div>
             </a>
         </div>
