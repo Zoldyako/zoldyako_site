8db1f469f32f5ce9b08c62be6a8a1633b1299f2d
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-03-30 23:45:14 -0300

    update: removes sidebar for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
+// import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -31,7 +31,7 @@ export default function RootLayout({
         >
             <body className="h-full flex">
                 <Navbar />
-                <Sidebar />
+                {/*<Sidebar /> TO-DO: Re-add when functionality is ready*/}
                 {children}
             </body>
         </html>
