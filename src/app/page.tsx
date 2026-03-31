4481391277b644eb82ebe22d68e90b12388e0030
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-03-31 02:11:42 -0300

    update: navbar and sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,31 @@
 import Image from "next/image";
+import Profile_pic from "@/assets/images/profile_pic.webp"
 
 export default function Home() {
     return (
         <div className="py-4 px-8 flex flex-col gap-8">
-            <div className="border border-zinc-600">
-                <h1 className="text-4xl">hello world, I'm José!</h1>
-                <p>Full-Stack Web Developer</p>
-                <Image
-                    height={256}
-                    width={256}
-                    src="https://placehold.co/256x256/png"
-                    loading="lazy"
-                    alt="placeholder"
-                />
+            <div className="flex flex-col gap-4">
+                <h1 className="text-4xl">Hello world, I'm José!</h1>
+                <p>
+                    You'll not believe this but I'm... A full-stack web-developer!
+                    <br></br>Almost finished with university :)
+                </p>
             </div>
-            <div className="border border-zinc-600">
-                <h2 className="text-3xl">What's my stack?</h2>
+            <div>
+                <h2 className="text-3xl pb-2">What's my stack?</h2>
                 <ul>
-                    <li> - JavaScript / TypeScript</li>
-                    <li> - Next.js + Tailwind</li>
-                    <li> - Node.js</li>
+                    <li>🟆 JavaScript / TypeScript</li>
+                    <li>🟆 Next.js + Node.js</li>
+                    <li>🟆 Tailwind</li>
+                    <li>🟆 Express.js </li>
+                    <li>🟆 Expanding!</li>
                 </ul>
             </div>
-            <div className="border border-zinc-600">
-                <h2>What have I built?</h2>
+            <div className="">
+                <h2 className="text-3xl pb-2">What have I built?</h2>
                 <ul>
-                    <li> - This site</li>
+                    <li>🟆 This site</li>
+                    <li>🟆 Future things here :)</li>
                 </ul>
             </div>
         </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,11 +3,27 @@ export default function Sidebar() {
         <div className="h-full w-64 py-4 flex flex-col justify-start items-center gap-8 bg-zinc-700 border-r border-zinc-600">
             <h2 className="text-4xl font-bold">Zoldyako</h2>
             <ul className="flex flex-col gap-2 w-full">
-                <a href="#"><li className="h-10 bg-zinc-500 py-2 px-4">Button Placeholder</li></a>
-                <a href="#"><li className="h-10 bg-zinc-500 py-2 px-4">Button Placeholder</li></a>
-                <a href="#"><li className="h-10 bg-zinc-500 py-2 px-4">Button Placeholder</li></a>
-                <a href="#"><li className="h-10 bg-zinc-500 py-2 px-4">Button Placeholder</li></a>
+                <a href="#">
+                    <li className="h-10 bg-zinc-500 py-2 px-4">
+                        Button Placeholder
+                    </li>
+                </a>
+                <a href="#">
+                    <li className="h-10 bg-zinc-500 py-2 px-4">
+                        Button Placeholder
+                    </li>
+                </a>
+                <a href="#">
+                    <li className="h-10 bg-zinc-500 py-2 px-4">
+                        Button Placeholder
+                    </li>
+                </a>
+                <a href="#">
+                    <li className="h-10 bg-zinc-500 py-2 px-4">
+                        Button Placeholder
+                    </li>
+                </a>
             </ul>
         </div>
-    )
+    );
 }
