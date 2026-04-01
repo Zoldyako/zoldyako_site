65c98854cfaffb32740d75ac703e61e592e2c280
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-04-01 15:18:52 -0300

    update: landing page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
-import Image from "next/image";
-import Profile_pic from "@/assets/images/profile_pic.webp"
-
 export default function Home() {
     return (
         <div className="py-4 px-8 flex flex-col gap-8">
             <div className="flex flex-col gap-4">
                 <h1 className="text-4xl">Hello world, I'm José!</h1>
                 <p>
-                    You'll not believe this but I'm... A full-stack web-developer!
+                    You'll not believe this but I'm... A full-stack
+                    web-developer!
                     <br></br>Almost finished with university :)
                 </p>
+                <div>
+                    <h2 className="text-3xl pb-2">My links:</h2>
+                    <ul>
+                        <li>
+                            🟆
+                            <a
+                                href="https://github.com/Zoldyako"
+                                target="_blank"
+                            >
+                                Github
+                            </a>
+                        </li>
+                        <li>
+                            🟆
+                            <a
+                                href="https://www.linkedin.com/in/jose-vdr/"
+                                target="_blank"
+                            >
+                                Linkedin
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div>
                 <h2 className="text-3xl pb-2">What's my stack?</h2>
