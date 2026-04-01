87cd00ef8c6738d10871e057255f4de704cfae92
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-04-01 15:20:58 -0300

    update: navbar.tsx because I was being dumb

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,7 @@ export default function Navbar() {
     return (
         <div className="w-18 min-w-18 h-svh flex flex-col gap-4 items-center py-6 bg-zinc-800 border-r border-zinc-600">
             <a href="#">
-                <div>
-                    <button className="hover:cursor-pointer">
-                        <House size={28} strokeWidth={2} />
-                    </button>
-                </div>
+                <House size={28} strokeWidth={2} />
             </a>
         </div>
     );
