d341d953f0d91301fc8536568527007ee5f85779
José Victor Dias Rodrigues <zoldyako@gmail.com>
2026-04-04 07:50:47 -0300

    add: time to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+    const date = new Date();
+    const [time, set_time] = useState({
+        hours: date.getHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            set_time((prevTime) => ({
+                ...prevTime,
+                hours: now.getHours(),
+                minutes: now.getMinutes(),
+                seconds: now.getSeconds(),
+            }));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const { hours, minutes, seconds } = time;
+
     return (
         <div className="py-4 px-8 flex flex-col gap-8">
             <div className="flex flex-col gap-4">
@@ -44,12 +71,20 @@ export default function Home() {
                     <li>🟆 Expanding!</li>
                 </ul>
             </div>
-            <div className="">
+            <div>
                 <h2 className="text-3xl pb-2">What have I built?</h2>
                 <ul>
                     <li>🟆 This site</li>
                     <li>🟆 Future things here :)</li>
                 </ul>
+            </div>
+            <div>
+                <h2 className="text-3xl pb-2">Time</h2>
+                <p>
+                    🟆 {hours < 10 ? "0" + hours : hours}:
+                    {minutes < 10 ? "0" + minutes : minutes}:
+                    {seconds < 10 ? "0" + seconds : seconds}
+                </p>
             </div>
         </div>
     );
